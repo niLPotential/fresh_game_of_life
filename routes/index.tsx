20e856a47f8a32d22aff6d1a83c6fcf7eb7ce36a
niLPotential <niLPotential@proton.me>
2023-03-04 01:19:29 +0900

    center and size title

--- routes/index.tsx
+++ routes/index.tsx
@@ -5,49 +5,51 @@ import IconBrandGithub from "https://deno.land/x/tabler_icons_tsx@0.0.2/tsx/bran
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-screen justify-center items-center font-mono bg-gray-200 dark:bg-gray-700 dark:text-white">
+    <>
       <Head>
         <title>Fresh Game of Life</title>
       </Head>
-      <div className="mb-5">
-        <a
-          className="text-2xl hover:text-blue-500"
-          href="https://en.wikipedia.org/wiki/Conway's_Game_of_Life"
-          target="_blank"
-        >
-          Conway's Game of Life
-        </a>
-        <br />
-        <a
-          className="hover:text-blue-500"
-          href="https://www.freecodecamp.org/news/coding-the-game-of-life-with-react-7de2385b7356/"
-          target="_blank"
-        >
-          Based on this freeCodeCamp article
-        </a>
-      </div>
-
-      <div>
-        <GameOfLife />
-
-        <div className="flex justify-around items-center">
-          <DarkMode />
-          <a href="https://fresh.deno.dev" target="_blank">
-            <img
-              width="197"
-              height="37"
-              src="https://fresh.deno.dev/fresh-badge-dark.svg"
-              alt="Made with Fresh"
-            />
-          </a>
+      <div className="h-screen flex flex-col justify-center items-center font-mono bg-gray-200 dark:bg-gray-700 dark:text-white">
+        <div className="mb-2 sm:mb-3 flex flex-col items-center">
           <a
-            href="https://github.com/niLPotential/fresh_game_of_life"
+            className="text-2xl sm:text-3xl hover:text-blue-500"
+            href="https://en.wikipedia.org/wiki/Conway's_Game_of_Life"
             target="_blank"
           >
-            <IconBrandGithub />
+            Conway's Game of Life
+          </a>
+          <br />
+          <a
+            className="sm:text-xl hover:text-blue-500"
+            href="https://www.freecodecamp.org/news/coding-the-game-of-life-with-react-7de2385b7356/"
+            target="_blank"
+          >
+            Based on this freeCodeCamp article
           </a>
         </div>
+
+        <div>
+          <GameOfLife />
+
+          <div className="flex justify-around items-center">
+            <DarkMode />
+            <a href="https://fresh.deno.dev" target="_blank">
+              <img
+                width="197"
+                height="37"
+                src="https://fresh.deno.dev/fresh-badge-dark.svg"
+                alt="Made with Fresh"
+              />
+            </a>
+            <a
+              href="https://github.com/niLPotential/fresh_game_of_life"
+              target="_blank"
+            >
+              <IconBrandGithub />
+            </a>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
