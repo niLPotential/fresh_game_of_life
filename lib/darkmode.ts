ca0cee76016bf33ba7e540139cbdd7c6f12c5833
niLPotential <niLPotential@proton.me>
2023-03-09 22:56:29 +0900

    Delete reference to local storage

--- lib/darkmode.ts
+++ lib/darkmode.ts
@@ -1,19 +1,12 @@
 import { effect, signal } from "@preact/signals";
 
-export const isDarkMode = signal(
-  (typeof localStorage !== undefined && localStorage.theme === "dark") ||
-    (!("theme" in localStorage) &&
-      typeof window.matchMedia !== "undefined" &&
-      window.matchMedia("(prefers-color-scheme: dark)").matches),
-);
+export const isDarkMode = signal(false);
 
 effect(() => {
   if (typeof document !== "undefined") {
     if (isDarkMode.value) {
-      localStorage.theme = "dark";
       document.documentElement.classList.add("dark");
     } else {
-      localStorage.theme = "light";
       document.documentElement.classList.remove("dark");
     }
   }
