6a6a276d2d55a41e357326017b79e7aeac95e5a6
niLPotential <niLPotential@proton.me>
2023-06-15 01:15:50 +0900

    Update fresh and deps

--- twind.config.ts
+++ twind.config.ts
@@ -1,6 +1,10 @@
-import { Options } from "$fresh/plugins/twind.ts";
+import { defineConfig } from "@twind/core";
+import presetAutoprefix from "https://esm.sh/@twind/preset-autoprefix@1.0.7";
+import presetTailwind from "https://esm.sh/@twind/preset-tailwind@1.1.4";
 
 export default {
+  ...defineConfig({
+    presets: [presetAutoprefix(), presetTailwind()],
+  }),
   selfURL: import.meta.url,
-  darkMode: "class",
-} as Options;
+};
